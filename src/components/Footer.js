abedeb5a23facef370a6ef0bf1733bd0ac7b21ad
jsmcallister98 <jakemcallister@outlook.com>
2021-01-15 22:42:52 -0800

    footer updates

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import { jsx } from 'theme-ui'
 import React from 'react'
 import Link from 'next/link'
 import { Flex } from 'theme-ui'
+import * as FaIcons from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -12,16 +13,23 @@ const Footer = () => {
         display: 'flex',
         flexWrap: 'wrap',
         alignItems: 'center',
+        justifyContent: 'space-around',
         p: 2,
+        height: 70,
+        bg: '#606060',
+        color: 'muted'
       }}>
       <Link href="/">
         <a>Home</a>
       </Link>
-      <Link href="/">
+      <Flex sx={{justifyContent: 'space-between', width: 150}}>
+        <a href="#" target="_blank"><FaIcons.FaInstagram sx={{ fontSize: '1.5rem'}}/></a>
+        <a href="#" target="_blank"><FaIcons.FaGithub sx={{ fontSize: '1.5rem'}}/></a>
+        <a href="#" target="_blank"><FaIcons.FaLinkedin sx={{ fontSize: '1.5rem'}}/></a>
+      </Flex>
+      <Link href="/contact-us">
         <a>Contact</a>
       </Link>
-      <div sx={{ mx: 'auto' }} />
-      <div sx={{ p: 2 }}>© 2019 Jane Doe</div>
     </footer>
   )
 }
